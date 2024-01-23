a4c81f8b252b74c8d309913eccfaf9c07ca77fe9
Nguyễn Quốc Toàn <nqt271@gmail.com>
2024-01-23 17:42:47 +0700

    add rsi and update data

--- src/processing/data_cleaning.py
+++ src/processing/data_cleaning.py
@@ -1,14 +1,38 @@
+import sqlite3
+
 import pandas as pd
 
 # Đọc dữ liệu từ file CSV
-df = pd.read_csv('../../data/raw_data/stock_ibm.csv')
+df_stock = pd.read_csv('../../data/raw_data/stock_ibm.csv')
 # sửa tên cột
-df.columns = ["Date","Stock", "Open", "High", "Low", "Close", "Volume"]
+df_stock.columns = ["Date","Stock", "Open", "High", "Low", "Close", "Volume"]
 
-df["Volatility"] = df["High"] - df["Low"]
+df_stock["Volatility"] = round(df_stock["High"] - df_stock["Low"], 4)
 
-print(df.head())
+print(df_stock.head())
 
+
+# Đọc dữ liệu từ file CSV
+df_rsi = pd.read_csv('../../data/raw_data/rsi_ibm.csv')
+
+print(df_rsi.head())
+#thêm cột rsi vào df_stock
+df_stock["RSI"] = df_rsi["RSI"]
+
+#xử lý dữ liệu thiếu
+df_stock["RSI"] = df_stock["RSI"].fillna(df_stock["RSI"].mean())
+
+# Kết nối đến SQLite
+conn = sqlite3.connect('stock_data.db')
+
+# Tạo bảng nếu chưa tồn tại
+df_stock.to_sql('stock_data', conn, if_exists='replace')
+
+# Lưu các thay đổi và đóng kết nối
+conn.commit()
+conn.close()
+
+print("Dữ liệu đã được lưu vào SQLite.")
 
 # Lưu trữ phiên bản đã xử lý của dữ liệu
-df.to_csv('../../data/cleaned_data/stock_ibm.csv')
+df_stock.to_csv('../../data/cleaned_data/stock_ibm.csv')
