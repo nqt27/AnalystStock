c4d9f3d3a73d5e803472782ed3e53b2d763b9e62
Nguyễn Quốc Toàn <nqt271@gmail.com>
2024-01-24 23:52:37 +0700

    update data

--- src/processing/data_cleaning.py
+++ src/processing/data_cleaning.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import pandas as pd
 
 # Đọc dữ liệu từ file CSV
@@ -5,10 +7,20 @@ df = pd.read_csv('../../data/raw_data/stock_ibm.csv')
 # sửa tên cột
 df.columns = ["Date","Stock", "Open", "High", "Low", "Close", "Volume"]
 
-df["Volatility"] = df["High"] - df["Low"]
+df["Volatility"] = round(df["High"] - df["Low"], 4)
 
 print(df.head())
 
+
+# Kết nối đến SQLite
+conn = sqlite3.connect('stock_data.db')
+
+# Tạo bảng nếu chưa tồn tại
+df.to_sql('stock_data', conn, if_exists='replace')
+
+# Lưu các thay đổi và đóng kết nối
+conn.commit()
+conn.close()
 
 # Lưu trữ phiên bản đã xử lý của dữ liệu
 df.to_csv('../../data/cleaned_data/stock_ibm.csv')
